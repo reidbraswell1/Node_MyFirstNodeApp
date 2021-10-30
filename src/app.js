b115a42faec2333c956e2ce53b6d07cce2859684
Reid Braswell <reidbras@gmail.com>
2021-10-30 16:42:36 -0500

    Adding code to create a server and send back a test response

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-// const http = require("http");
-import http from ('http');
-import path from ('path');
+//const http = require("http");
+import http from 'http';
+import path from 'path';
+import ejs from 'ejs';
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end("<h1>Hello World</h1>");
+    //res.end(JSON.stringify({
+      //data: 'Hello World!'
+    //}));
+  });
+  
+  server.listen(8000);
