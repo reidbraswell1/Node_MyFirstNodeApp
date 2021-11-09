027a2b4ac6e390fc52b2a9564bb28b4c0d561ce9
Reid Braswell <reidbras@gmail.com>
2021-11-09 16:47:15 -0600

    Adding detection of form-submission without entering form beforehand

--- src/app.js
+++ src/app.js
@@ -41,55 +41,6 @@ const server = http.createServer((req, res) => {
     case '/form-submission':
       console.log(`--- Begin Case ${urlToRoute} Route ---`);
       formSubmissionProcess(req, res);
-      /*
-      switch(req.method) {
-        case "GET":
-          console.log(`--- Begin Case ${req.method}`);
-          console.log(queryObject);
-          console.log(`Query Object Name = ${queryObject.name}`);
-          console.log("\nMethod 2 URLSearchParms");
-          let params = new URLSearchParams(req.url.substring(req.url.indexOf("?")));
-          console.log(`GET URL Parameters = ${req.url.substring(req.url.indexOf("?"))}`)
-          console.log(`GET URLSearchParams Parameters = ${params}`);
-          console.log(`GET URLSearchParams.name = ${params.get("name")}\n`);
-          console.log(`GET URLSearchParams.favorite-programming-language = ${params.get("favorite-programming-language")}\n`);
-          console.log(`--- End Case ${req.method} ---`);
-          break;
-        case "POST":
-          console.log(`--- Begin Case ${req.method}`);
-          let body = '';
-          // Errors
-          req.on('error', (err) => {
-            if(err) {
-              res.writeHead(500, {'Content-Type': 'text/html'});
-              res.write('An error occurred');
-              res.end();
-            }
-          });
-          // read chunks of POST data
-          req.on('data', chunk => {
-            body += chunk.toString();
-          });
-          // when complete POST data is received
-          req.on('end', () => {
-            // use parse() method
-            //body = querystring.parse(body);
-            postObject = new URLSearchParams(`?${body}`);
-            console.log(`POST body = ${body}`);
-            console.log(`POST Object = ${postObject}`);
-            console.log(`POST URLSearchParams.name = ${postObject.get("name")}`);
-            console.log(`POST URLSearchParams.favorite-programming-language = ${postObject.get("favorite-programming-language")}`);
-            htmlFile = 'response.ejs';
-            type = 'text/html';
-            data = postObject;
-            console.log(`post objecet before render = ${postObject}`);
-            render(res,req,"response.ejs",'text/html',postObject);
-          });
-          console.log(`--- End Case ${req.method} ---`);
-          break;
-        }
-        */
-
         console.log(`--- End Case ${urlToRoute} Route ---`);
         break;
       case '/styles/indexStyle.css':
@@ -111,66 +62,9 @@ const server = http.createServer((req, res) => {
         console.log(`--- Begin Case default ${urlToRoute} Route ---`);
         //console.log(querystring.parse(req.url));
         oopsPage(req, res);
-        /*
-        htmlFile = 'oops.html';
-        type = 'text/html';
-        */
         console.log(`--- End Case ${urlToRoute} ---`);
         break;
-  }
-
-  if(htmlFile) {
-    console.log(`if htmlFile entered`);
-		render(res, req, htmlFile, type, data);
-  }
-  /*
-
-  function render(res,req, htmlFile, type, data) {
-    console.log(`--- Begin Function render() ${htmlFile}---`);
-    switch (type) {
-      // Serve a html file
-      case 'text/html':
-        console.log(`--- Begin Case ${type} ---`);
-        fs.stat(`./views/${htmlFile}`, (err, stats) => {
-          res.statusCode = 200;
-          // Dont set content type if using ejs
-          //res.setHeader('Content-Type', 'text/html');
-          //console.log(err);
-          if(stats) {
-            const template = fs.readFileSync(`./views/${htmlFile}`,'utf-8');
-            let renderedTemplate;
-            //console.log(htmlFile);
-            if(htmlFile == 'response.ejs') {
-              renderedTemplate = ejs.render(template,{title:'Form Response',name:data.get("name"), favoriteProgrammingLanguage:data.get("favorite-programming-language")});
-            }
-            else {
-              renderedTemplate = ejs.render(template,{title:"apple"});
-            }
-            //fs.createReadStream(`./views/${htmlFile}`).pipe(res);
-            res.end(renderedTemplate);
-          }
-          else {
-            res.statusCode = 404;
-            res.end('<h1>Sorry, page not found!</h1>');
-          }
-          console.log(`--- End Case ${type} ---`);
-        });
-        break;
-        // Serve a css file
-      case 'text/css':
-        console.log(`--- Begin Case ${type} ---`);
-        let fileStream = fs.createReadStream(`./styles/${htmlFile}`, "utf-8");
-        let css = fs.readFileSync(`./styles/${htmlFile}`, "utf-8");
-        //console.log(fileStream);
-        res.writeHead(200, {"Content-Type": "text/css"});
-        res.end(css);
-        //fileStream.pipe(res);
-        console.log(`--- End Case ${type} ---`);
-        break;
-      } // Switch
-        console.log(`---End Function render()---`);
-    } // Render
-    */
+    }
   });
   server.listen(8000);
   console.log("Server running at http://localhost:8000/");
@@ -246,7 +140,16 @@ function formSubmissionProcess(req, res) {
       console.log(`GET URLSearchParams.name = ${params.get("name")}\n`);
       console.log(`GET URLSearchParams.favorite-programming-language = ${params.get("favorite-programming-language")}\n`);
       console.log(`--- End Case ${req.method} ---`);
-      responsePage(req, res, params);
+      if(params.get("name") === null || params.get("name") === undefined || 
+         params.get("favoriteProgrammingLanguage") === null || params.get("favoriteProgrammingLanguage") === undefined) {
+        res.writeHead(302, {
+          location: "/about",
+        });
+        return res.end();
+      }
+      else {
+        responsePage(req, res, params);
+      }
       break;
     case "POST":
       console.log(`--- Begin Case ${req.method}`);
@@ -273,8 +176,18 @@ function formSubmissionProcess(req, res) {
         console.log(`POST URLSearchParams.name = ${postObject.get("name")}`);
         console.log(`POST URLSearchParams.favorite-programming-language = ${postObject.get("favorite-programming-language")}`);
         console.log(`post objecet before render = ${postObject}`);
-        responsePage(req, res, postObject);
-        //render(res,req,"response.ejs",'text/html',postObject);
+        if (postObject.get("name") === null && postObject.get("favoriteProgrammingLanguage") === null) {
+          console.log(postObject.get("name"));
+          console.log("here1");
+          res.writeHead(302, {
+            location: "/about",
+          });
+          return res.end();
+        }
+        else {
+          console.log("here");
+          responsePage(req, res, postObject);
+        }
       });
   }
   console.log(`--- End Function formSubmissionProcess() ---`);
